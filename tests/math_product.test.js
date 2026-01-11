b01cedcd89cf490e53448bd59ca95577ebea3cd8
Aleksiej Kosenko <aleksejkosenko352@gmail.com>
2026-01-11 14:09:47 +0200

    Break test to check CI failure

--- tests/math_product.test.js
+++ tests/math_product.test.js
@@ -2,6 +2,6 @@ const multiply = require('../src/math_product');
 
 test('multiplication works correctly', () => {
   expect(multiply(2, 3)).toBe(6);
-  expect(multiply(-4, 5)).toBe(-20);
+  expect(multiply(-4, 5)).toBe(-22); // <- помилка навмисно
   expect(multiply(0, 10)).toBe(0);
 });
